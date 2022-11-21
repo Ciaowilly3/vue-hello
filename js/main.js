f7c02c96d2a3b0b2c4e62287d8e57f11ec6f4216
Ciaowilly3 <muscomassi@gmail.com>
2022-11-21 13:44:26 +0100

    aggiunto src con vue

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ createApp({
     return {
       scritta: "Made using Vue",
       classiScritta: "text-primary text-center",
+      srcImmagine: `https://www.rainews.it/dl/img/2022/05/29/1653823359813_Immagine__.png`,
+      classiImmagine: "m-auto d-block",
     };
   },
 }).mount("#app");
