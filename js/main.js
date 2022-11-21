f0c8324602fc1a50d01af1c5b179ad80d0a52a86
Ciaowilly3 <muscomassi@gmail.com>
2022-11-21 13:33:06 +0100

    scritta creata

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      scritta: "Made using Vue",
+      classiScritta: "text-primary text-center",
+    };
+  },
+}).mount("#app");
